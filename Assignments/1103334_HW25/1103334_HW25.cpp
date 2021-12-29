1d4f64cdb95b3d0b56fd0ccc36c5809af6c8be2c
mhy1264 <s710516@student2.ylsh.chc.edu.tw>
2021-12-29 20:44:15 +0800

    Modify HW25

--- Assignments/1103334_HW25/1103334_HW25.cpp
+++ Assignments/1103334_HW25/1103334_HW25.cpp
@@ -1,17 +1,7 @@
-/*
-姓名 :楊明寰
-學號 : 1103334
-----------------
-
-就是一開始先固定字串的第一個字
-剩下的幾個字在先固定第一格字 之後交換 輸出
-以此類推
-
-*/
-
 #include <string>
 #include <iostream>
 using namespace std;
+
 void permute(string a, int l, int r)
 {
 	if (l == r)
@@ -22,7 +12,6 @@ void permute(string a, int l, int r)
 		{
 			swap(a[l], a[i]);
 			permute(a, l + 1, r);
-			swap(a[l], a[i]);
 		}
 	}
 }
