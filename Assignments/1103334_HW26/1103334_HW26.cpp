1cb978904da302e9e274457b4105e1e09d657c49
mhy1264 <49542664+mhy1264@users.noreply.github.com>
2022-01-05 18:34:34 +0800

    Add HW 26

--- Assignments/1103334_HW26/1103334_HW26.cpp
+++ Assignments/1103334_HW26/1103334_HW26.cpp
@@ -1,7 +1,37 @@
-// 雙色何內塔
 #include<iostream>
 using namespace std;
+
+void hanoi(int n, char A, char B, char C)
+{
+	if (n == 1)
+		cout << "Move " << n << " from " << A << " to " << C << endl;
+	else
+	{
+		hanoi(n - 1, A, C, B);
+		cout << "Move " << n << " from " << A << " to " << C << endl;
+		hanoi(n - 1, B,A,C);
+	}
+}
+void twohanoi(int n, char A, char B, char C)
+{
+    if(n==1)
+    {
+        cout << "Move " << n << " from " << A << " to " << B << endl;
+        cout << "Move " << n << " from " << A << " to " << C << endl;
+    }
+    else
+    {
+        hanoi(n - 1, A, B, C);
+       cout << "Move " << n << " from " << A << " to " << B << endl;
+        hanoi(n - 1, C, A, B);
+       cout << "Move " << n << " from " << A << " to " << C << endl;
+        hanoi(n - 1, B, A, C);
+    }
+}
+
 int main()
 {
-	
+    int n;
+    while(cin>>n)
+	    twohanoi(n,'A','B','C');
 }
