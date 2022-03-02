17570a6f988eff55605779ee41815619f70cd89d
Ming Huan Yang <s710516@student2.ylsh.chc.edu.tw>
2022-03-02 18:59:44 +0800

    Update Assignment 3
    sort () -> DoSort()

--- 1102/Assignments/1103334-HW03/1103334-HW03.cpp
+++ 1102/Assignments/1103334-HW03/1103334-HW03.cpp
@@ -22,17 +22,39 @@ int Book::get()
     return price;
 }
 
-
+void DoSort(int n, int* ptr)
+{
+    int i, j, t;
+  
+    // Sort the numbers using pointers
+    for (i = 0; i < n; i++) {
+  
+        for (j = i + 1; j < n; j++) {
+  
+            if (*(ptr + j) < *(ptr + i)) {
+  
+                t = *(ptr + i);
+                *(ptr + i) = *(ptr + j);
+                *(ptr + j) = t;
+            }
+        }
+    }
+  
+}
 int main()
 {
     const int n =10;
     srand(time(NULL));
     Book books[n];
+    int arr[n];
 
     for (int i=0;i<n;i++)
-        books[i].set(rand() %1000);
+        arr[i]=rand() %1000;
 
-    sort(books,books+n);
+    DoSort(n,arr);
+
+    for(int i=0;i<n;i++)
+    books[i].set(arr[i]);
 
     for(int i=0;i<n;i++)
        cout<<books[i].get()<<" ";
