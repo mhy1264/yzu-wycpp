3cea36d87bdb1c48d953813c5cfeb78a9bebbe94
mhy1264 <49542664+mhy1264@users.noreply.github.com>
2022-03-02 11:45:44 +0800

    Update 1103334-HW1.cpp

--- 1102/Assignments/1103334-HW01/1103334-HW1.cpp
+++ 1102/Assignments/1103334-HW01/1103334-HW1.cpp
@@ -10,9 +10,13 @@ int main()
         if (in == '0')
             break;
         arr[(int)in]++;
+        // 先把輸入的 char 轉為 int ，例如 '0' 轉為 48
+        // 然後再把arr的第48項+1
     }
     while (cin >> in)
     {
+        // 確認arr的第 (int)in 項是否為零 
+        // 如果不為零 -> 輸出存在後歸0
         if (arr[(int)in]) // is in arr
         {
             cout << "Exist" << endl;
