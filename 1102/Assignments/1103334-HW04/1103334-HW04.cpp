37b0850e07107019545c615767b239bc4921a871
Ming Huan Yang <49542664+mhy1264@users.noreply.github.com>
2022-03-06 23:05:28 +0800

    Add comment 1103334-HW04.cpp

--- 1102/Assignments/1103334-HW04/1103334-HW04.cpp
+++ 1102/Assignments/1103334-HW04/1103334-HW04.cpp
@@ -4,7 +4,9 @@ using namespace std;
 class Rectangle
 {
 public:
+    // 劃出圖型
     void draw(int, int);
+    // 設定長寬
     void set(int, int);
 private:
     int h;
@@ -13,26 +15,25 @@ private:
 };
 
 void Rectangle::set(int x, int y)
-{
     h = x;
     w = y;
 }
-void Rectangle::draw(int h, int w)
+void Rectangle::draw(int h, int w) 
 {
-    for (int i = 0; i < w; i++)
+    for (int i = 0; i < w; i++) // 最上方的邊界
         cout << "*";
     cout << endl;
 
-    for (int j = 0; j < h - 2; j++)
+    for (int j = 0; j < h - 2; j++) // 扣掉上下兩行
     {
         cout << "*";
-        for (int i = 0; i < w - 2; i++)
+        for (int i = 0; i < w - 2; i++) // 扣掉左右兩個*
             cout << " ";
         cout << "*" << endl;;
     }
 
 
-    for (int i = 0; i < w; i++)
+    for (int i = 0; i < w; i++) //下邊界
         cout << "*";
     cout << endl;
 }
@@ -42,8 +43,8 @@ int main()
     int w, h;
     while (cin >> w >> h)
     {
-        Rectangle obj;
-        obj.set(h, w);
-        obj.draw(h, w);
+        Rectangle obj; // 宣告一個物件
+        obj.set(h, w); // 設定長寬
+        obj.draw(h, w); // 開始畫
     }
 }
