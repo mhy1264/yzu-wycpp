6c5d9eca872e38d724bcde8cebe3f3a3797399d1
Ming Huan Yang <s710516@student2.ylsh.chc.edu.tw>
2022-05-13 23:34:56 +0800

    Add Assignment-16

---
+++ 1102/Assignments/1103334-HW16/1103334-HW16.cpp
@@ -0,0 +1,115 @@
+#include<iostream>
+using namespace std;
+
+int id = 0;
+
+class Person
+{
+public:
+
+	void getData()
+	{
+		cout << _id << " " << _name << " " << _age << " ";
+	}
+
+	void putData()
+	{
+		cin>>_name;
+		cin >> _age;
+		_id = ++id;
+	}
+
+protected:
+	char _name[10];
+	int _age;
+	int _id;
+
+};
+
+class Professor :public Person
+{
+public:
+
+	void getData()
+	{
+		Person::getData();
+		cout << _publications << endl;;
+	}
+
+	void putData()
+	{
+		Person::putData();
+		cin >> _publications;
+	}
+private:
+	int _publications;
+};
+
+class Student :public Person
+{
+public:
+
+	void getData()
+	{
+		int sum = 0;
+
+		for (int i = 0; i < 6; i++)
+			sum += _marks[i];
+
+		Person::getData();
+		cout << sum << endl;;
+	}
+
+	void putData()
+	{
+		
+		Person::putData();
+
+		for (int i = 0; i < 6; i++)
+			cin >> _marks[i];
+	}
+
+private:
+	int _marks[6];
+};
+
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	int* loc = new int[n]();
+	Professor* pro = new Professor[n];
+	Student* stu = new Student[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		int t;
+		cin >> t;
+		if (t == 2)
+		{
+			loc[i] = 1;
+			Student temp;
+			temp.putData();
+			stu[i] = temp;
+		}
+		if (t==1)
+		{
+			loc[i] = 2;
+			Professor temp;
+			temp.putData();
+			pro[i] = temp;
+		}
+
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (loc[i] == 1)
+			stu[i].getData();
+		if (loc[i] == 2)
+			pro[i].getData();
+	}
+
+}
